626d27b38d5dd71bf049bd98ea3a46f73fd615c7
ajay jha <akjha@IN-AKJHA-MP.local>
2020-08-12 21:54:56 +0530

    master: sms

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+export interface SmsListenerEvent {
+  message?: string
+  extras?: string
+  status?: string
+  timeout?: string
+}
+
+declare const SmsRetrieverModule: {
+  requestPhoneNumber: () => Promise<string>
+  startSmsRetriever: () => Promise<boolean>
+  addSmsListener: (callback: (event: SmsListenerEvent) => void) => Promise<boolean>
+  removeSmsListener: () => void,
+  getAppSignature: () => Promise<string>
+}
+
+export default SmsRetrieverModule
